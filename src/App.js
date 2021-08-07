e3869b6e4be1a7650da33b0a5fe33ca067ac66be
katherine lizardo <lizardokm@gmail.com>
2021-08-07 20:46:39 -0300

    fonts+icon+bootstrap+test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {BrowserRouter as Router } from "react-router-dom"
+import AppRouter from './routers/AppRouter'
+import Navigation from './components/Navigation'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       Task Manager - Final Project Toti
-      </header>
-    </div>
+          <>
+          <div>
+            <Router>
+                  <Navigation/>
+                  <AppRouter/>
+            </Router> 
+          </div>
+          </>
   );
 }
 
---
+++ src/routers/AppRouter.js
@@ -0,0 +1,9 @@
+
+
+export default function AppRouter() {
+    return (
+        <div>
+            Rutas
+        </div>
+    )
+}
